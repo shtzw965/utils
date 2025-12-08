6b2a1d0e0d2ed681e89221b636aea10d5ae33ede
shtzw965 <129487081+shtzw965@users.noreply.github.com>
2025-12-08 12:23:23 +0800

    NUL

---
+++ android/adb.sh
@@ -0,0 +1,5 @@
+settings put global ntp_server time.edu.cn
+settings put global auto_time 1
+settings put global captive_portal_http_url http://www.google.cn/generate_204
+settings put global captive_portal_https_url https://www.google.cn/generate_204
+settings put global captive_portal_mode 1
