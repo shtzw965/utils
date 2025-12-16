757695944a42a36a7c8a7e00f803906dc5c47f22
shtzw965 <129487081+shtzw965@users.noreply.github.com>
2025-12-16 15:22:41 +0800

    NUL

--- py/ns.py
+++ py/ns.py
@@ -1,29 +1,28 @@
 #!/bin/python3
 import os, ctypes, errno
-libc = ctypes.CDLL(None, use_errno = True)
 exec('from ctypes import byref, ' + ', '.join([] + [i for i in dir(ctypes) if i.startswith('c')]))
+libc = ctypes.CDLL(None, use_errno = True)
 libc.mount.argtypes, libc.mount.restype = [c_void_p, c_void_p, c_void_p, c_ulong, c_void_p], c_int
 libc.umount.argtypes, libc.umount.restype = [c_void_p, c_int], c_int
 MS_REC = 1 << 14
 MS_PRIVATE = 1 << 18
+MS_SLAVE = 1 << 19
+MS_SHARED = 1 << 20
 uid, gid = os.getuid(), os.getgid()
 os.unshare(os.CLONE_NEWNS | os.CLONE_NEWUSER)
 fd = os.open('/proc/self/setgroups', os.O_WRONLY)
 os.write(fd, b'deny')
 os.close(fd)
 fd = os.open('/proc/self/uid_map', os.O_WRONLY)
-os.write(fd, b' '.join([b'0', str(uid).encode(), b'1']))
+os.write(fd, b'0 %i 1' % uid)
 os.close(fd)
 fd = os.open('/proc/self/gid_map', os.O_WRONLY)
-os.write(fd, b' '.join([b'0', str(gid).encode(), b'1']))
+os.write(fd, b'0 %i 1' % gid)
 os.close(fd)
-r = libc.mount(0, b'/', 0, MS_REC | MS_PRIVATE, 0)
-if 0 != r:
-  e = ctypes.get_errno()
-  raise OSError(e, os.strerror(e))
+assert libc.mount(0, b'/', 0, MS_REC | MS_SLAVE, 0) == 0
 
 os.execvp('csh', ['csh'])
-# mount -t overlay overlay -o rw,lowerdir=lower,upperdir=rw,workdir=wk tagert
-# unshare --mount --user --map-root-user bash
-# unshare --mount --user --map-root-user --propagation private bash
-# --propagation slave|shared|private
+# unshare --mount --user --map-root-user --propagation slave csh
+# mkdir rd rw wk merge
+# mount -t overlay overlay -o rw,lowerdir=rd,upperdir=rw,workdir=wk merge
+# rm -rf rd rw wk merge
